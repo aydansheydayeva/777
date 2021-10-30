1379f9e5089380c8df63d551a38c4a8dd5d8e9cb
aydansheydayeva <aydansheydayeva75@gmail.com>
2021-10-30 13:03:07 +0400

    adding lab7

--- src/test/java/com/example7/demo7/Demo7ApplicationTests.java
+++ src/test/java/com/example7/demo7/Demo7ApplicationTests.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import com.google.api.client.http.HttpHeaders;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -17,20 +18,27 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootTest
 class Demo7ApplicationTests {
 
+	@LocalServerPort
+    private int port;
+
+	@Test
+	void contextLoads() {
+	}
+
 	@Test
     public void testPOST(){
-        String url = "http://localhost:8080/api/users";
+        String url = "http://localhost:"+port+"/api/users";
         RestTemplate restTemplate = new RestTemplate();
 		Map<String, String> map = new HashMap<>();
 		map.put("username", "aysah");
 		map.put("password", "aysah!");
 
-        ResponseEntity<Void> response = restTemplate.postForEntity(url, map, Void.class);
+        restTemplate.postForEntity(url, map, Void.class);
 
 		System.out.println("Request Successful");
 
 
-		String url2 = "http://localhost:8080/api/users/aysah";
+		String url2 = "http://localhost:"+port+"/api/users/aysah";
 		HttpHeaders headers2 = HttpHeaders();
 		HttpEntity<Object> entity2 = new HttpEntity<Object>(headers2);
 		ResponseEntity<String> out2 = restTemplate.exchange(url2, HttpMethod.GET, entity2, String.class);

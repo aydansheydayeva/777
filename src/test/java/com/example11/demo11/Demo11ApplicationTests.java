cf90d5a1ce97a5e7e0e068d82895be7a4b91c7d3
aydansheydayeva <aydansheydayeva75@gmail.com>
2021-12-01 09:45:12 +0400

    adding lab11

---
+++ src/test/java/com/example11/demo11/Demo11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example11.demo11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
